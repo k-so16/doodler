3f2e4f8760999c7b36724acbaa623bb16a3b5aa9
Soichiro Kato <m17se004@m.nanzan-u.ac.jp>
2018-02-28 14:48:29 +0900

    changed view at if-else condition

--- resources/views/doodle/list.blade.php
+++ resources/views/doodle/list.blade.php
@@ -8,26 +8,24 @@
 
 @section('content')
 <!-- <a id="draw" href="{{ url('/draw') }}">Let's draw!</a> -->
+@if(count($items) > 0)
 <table class="doodles">
-  @if(count($items) > 0)
-    <tr>
-      <th></th>
-      <th>Title</th>
-      <th>Doodler</th>
-      <th>Created at</th>
-    </tr>
-    @foreach($items as $item)
-    <tr>
-      <td><img src="{{ $item->src }}"></td>
-      <td>{{ $item->title }}</td>
-      <td>{{ $item->doodler }}</td>
-      <td>{{ $item->created }}</td>
-    </tr>
-    @endforeach
-  @else
   <tr>
-    <td>No Doodles Registered.</td>
+    <th></th>
+    <th>Title</th>
+    <th>Doodler</th>
+    <th>Created at</th>
   </tr>
-  @endif
+  @foreach($items as $item)
+  <tr>
+    <td><img src="{{ $item->src }}"></td>
+    <td>{{ $item->title }}</td>
+    <td>{{ $item->doodler }}</td>
+    <td>{{ $item->created }}</td>
+  </tr>
+  @endforeach
 </table>
+@else
+<p>No Doodles Registered.</p>
+@endif
 @endsection
