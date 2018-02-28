64c1a2295e35086ad6738b14b99ce3896062e9a6
Soichiro Kato <m17se004@m.nanzan-u.ac.jp>
2018-02-28 21:45:41 +0900

    create a page displaying single doodle

---
+++ resources/views/doodle/appreciation.blade.php
@@ -0,0 +1,21 @@
+@extends('doodle')
+
+@section('res')
+<link rel="stylesheet" href="{{ asset('/css/doodle.php') }}">
+@endsection
+
+@section('content')
+<table>
+  <tr>
+    <td colspan="2"><img src=""></td>
+  </tr>
+  <tr>
+    <td>Title</td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>Doodler</td>
+    <td></td>
+  </tr>
+</table>
+@endsection
