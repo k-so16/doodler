fd734b88bda04303b3d94c814a85bbc5b7366a05
Soichiro Kato <m17se004@m.nanzan-u.ac.jp>
2018-03-01 15:08:53 +0900

    create a migration file and set its model

--- app/Doodle.php
+++ app/Doodle.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doodle extends Model
 {
-    //
+  protected $guarded = ['id'];
+
+  public static $rules = [
+    'title' => 'required',
+    'name' => 'required',
+    'file_path' => 'required',
+  ];
 }
