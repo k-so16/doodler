eb8dbffd75af2489bad3a3b08615c69d12852f4d
Soichiro Kato <m17se004@m.nanzan-u.ac.jp>
2018-03-01 15:28:17 +0900

    added data registering and fetchng function

--- app/Http/Controllers/DoodleController.php
+++ app/Http/Controllers/DoodleController.php
@@ -5,25 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 
+use App\Doodle;
+
 class DoodleController extends Controller
 {
-  public function index()
+  public function index(Request $request)
   {
-    $items = [
-      (object)[
-        'src' => 'http://azuki-penguin.org/drawing/crowbar.png',
-        'title' => 'crowbar',
-        'doodler' => 'so16',
-        'created' => '2018/2/27'
-      ],
-      (object)[
-        'src' => 'http://azuki-penguin.org/drawing/sakura.png',
-        'title' => 'cherry blossoms',
-        'doodler' => 'so16',
-        'created' => '2018/2/27'
-      ]
-    ];
-    return view('doodle.list', ['items' => $items]);
+    // dd(Doodle::all());
+    if(isset($request->id)) {
+      $data = Doodle::where('id', $request->id)->first();
+      if($data === null) {
+        return redirect('/');
+      }
+      return view('doodle.appreciation', ['data' => $data]);
+    } else {
+      $items = Doodle::all();
+      return view('doodle.list', ['items' => $items]);
+    }
   }
 
   public function draw()
@@ -35,5 +33,30 @@ class DoodleController extends Controller
       'colors' => Config::get('css.colors')
     ];
     return view('doodle.draw', $config);
+  }
+
+  public function register(Request $request)
+  {
+    $this->validate($request, [
+      'doodle' => [
+        'required',
+        'image'
+      ]
+    ]);
+
+    if($request->file('doodle')->isValid()) {
+      $filename = $request->doodle->store('images', 'public');
+      $doodle = new Doodle;
+      $formData = [
+        'title' => ($request->title) ? $request->title : "UNTITLED",
+        'name' => ($request->name) ? $request->name : "No Name",
+        'file_path' => $filename
+      ];
+      $doodle->fill($formData)->save();
+      return redirect('/');
+    } else {
+      return 'Error occured.';
+    }
+
   }
 }
