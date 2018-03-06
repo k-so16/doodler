39d545a950b4c516a3b4e13105cdc3097c17c86a
Soichiro Kato <m17se004@m.nanzan-u.ac.jp>
2018-03-06 16:15:59 +0900

    changed list sorting order by desc

--- app/Http/Controllers/DoodleController.php
+++ app/Http/Controllers/DoodleController.php
@@ -19,7 +19,7 @@ class DoodleController extends Controller
       }
       return view('doodle.appreciation', ['data' => $data]);
     } else {
-      $items = Doodle::all();
+      $items = Doodle::orderBy('created_at', 'desc')->get();
       return view('doodle.list', ['items' => $items]);
     }
   }
