d31412135deee6ef28d2baeb42924a18e7de58d0
Soichiro Kato <m17se004@m.nanzan-u.ac.jp>
2018-08-03 21:51:19 +0900

    changed algorithm to draw line   draw line from old to current coordinates as mousemove or touchmove

--- public/js/doodle.js
+++ public/js/doodle.js
@@ -1,117 +1,179 @@
-// window.onload = function() {
-$(function() {
-  var canvas  = $("canvas")[0];
-  var context = canvas.getContext('2d');
-  var isDrawing = false;
+var canvas = $("canvas");
+var option = { width: 3, height: 3, color: '#000000' };
+var oldPos = { x: null, y: null };
 
-  // begin to draw on mousedown.
-  canvas.onmousedown = function(e) {
-    if(!isDrawing) {
-      isDrawing = true;
-      var rect = e.target.getBoundingClientRect();
-      var x = e.clientX - rect.left;
-      var y = e.clientY - rect.top;
-      // console.log(`x: ${e.clientX}-${rect.left}=${x}`);
-      // console.log(`y: ${e.clientY}-${rect.top}=${y}`);
-      context.fillRect(x, y, 3, 3);
+// begin drawing
+canvas.on('mousedown', function (e) {
+  e.preventDefault();
+  // cancel drawing if any button but left was down
+  if (e.button !== 0) {
+    return;
+  }
 
-      canvas.onmousemove = function(e) {
-        var rect = e.target.getBoundingClientRect();
-        var x = e.clientX - rect.left;
-        var y = e.clientY - rect.top;
-        // console.log(`x: ${e.clientX}-${rect.left}=${x}`);
-        // console.log(`y: ${e.clientY}-${rect.top}=${y}`);
+  var rect = e.target.getBoundingClientRect();
+  var x = e.clientX - rect.left;
+  var y = e.clientY - rect.top;
 
-        context.fillRect(x, y, 3, 3);
-      }
+  pen_down(x, y);
+});
+
+canvas.on('touchstart', function(e) {
+  e.preventDefault();
+  // cancel drawing if 2 or more fingers touched on the canvas
+  if (e.touches.length !== 1) {
+    return;
+  }
+
+  var rect = e.target.getBoundingClientRect();
+  var x = e.touches[0].clientX - rect.left;
+  var y = e.touches[0].clientY - rect.top;
+
+  pen_down(x, y);
+});
+
+// drawing
+canvas.on('mousemove', function(e) {
+  e.preventDefault();
+  // cancel drawing if any button but left was down
+  if (e.buttons !== 1) {
+    return;
+  }
+
+  var rect = e.target.getBoundingClientRect();
+  var x = e.clientX - rect.left;
+  var y = e.clientY - rect.top;
+
+  draw(x, y);
+});
+
+canvas.on('touchmove', function (e) {
+  e.preventDefault();
+  // cancel drawing if 2 or more fingers touched on the canvas
+  if (e.touches.length !== 1) {
+    return;
+  }
+
+  var rect = e.target.getBoundingClientRect();
+  var x = e.touches[0].clientX - rect.left;
+  var y = e.touches[0].clientY - rect.top;
+
+  draw(x, y);
+});
+
+// end of drawing
+canvas.on('mouseup', function(e) {
+  pen_up();
+  canvas.off('mousemove', draw);
+});
+
+canvas.on('mouseout', function(e) {
+  pen_up();
+  canvas.off('mousemove', draw);
+});
+
+canvas.on('touchend', function(e) {
+  pen_up();
+  canvas.off('touchmove', draw);
+});
+
+// clear canvas
+$('#clear').on('click', function(e) {
+  canvas.clearCanvas();
+});
+
+// change pen color
+$('table.color_palette td').each(function() {
+  $(this).on('click', changeColor);
+});
+
+// change canvas background
+$('table.bg_palette input[name="canvas_bg"]').on('change', changeBg);
+
+// send doodle data
+$('#submit').on('click', function(e) {
+  var blob = canvas2blob($('canvas')[0]);
+  var formData = new FormData($('form')[0]);
+  formData.append('doodle', blob);
+
+  $.ajax({
+    type: 'POST',
+    data: formData,
+    processData: false,
+    contentType: false,
+    url: 'draw',
+    success: function(data, textStatus, jqXHR) {
+      window.location.href = './';
+    },
+    error: function(req, status, err) {
+      console.log('error');
+      alert('Sorry, error occured...');
     }
-  }
-
-  canvas.ontouchstart = function(e) {
-    e.preventDefault();
-
-    if(!isDrawing) {
-      isDrawing = true;
-      var rect = e.target.getBoundingClientRect();
-      var x = (e.touches[0]).clientX - rect.left;
-      var y = (e.touches[0]).clientY - rect.top;
-      // console.log(`x: ${e.clientX}-${rect.left}=${x}`);
-      // console.log(`y: ${e.clientY}-${rect.top}=${y}`);
-      context.fillRect(x, y, 3, 3);
-
-      canvas.ontouchmove = function(e) {
-        e.preventDefault();
-        var rect = e.target.getBoundingClientRect();
-        var x = (e.touches[0]).clientX - rect.left;
-        var y = (e.touches[0]).clientY - rect.top;
-        // console.log(`x: ${e.clientX}-${rect.left}=${x}`);
-        // console.log(`y: ${e.clientY}-${rect.top}=${y}`);
-
-        context.fillRect(x, y, 3, 3);
-      }
-    }
-  }
-
-  // end of drawing on mouseup
-  document.onmouseup = function() {
-    isDrawing = false;
-    canvas.onmousemove = null;
-  }
-
-  document.ontouchend = function() {
-    isDrawing = false;
-    canvas.ontouchmove = null;
-  }
-
-  // clear the canvas when clear button clicked.
-  $('#clear').on('click', function(e) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-  });
-
-  // change pen color when another color chosen
-  $('table.color_palette td').each(function() {
-    $(this).on('click', function() {
-      context.fillStyle = $(this).attr('class');
-    });
-  });
-
-  // change canvas background when another background chosen
-  $('table.bg_palette input[name="canvas_bg"]').on('change', function() {
-    var bg = $(this).val();
-    $(canvas).css('background', 'url("img/' + bg + '.png")');
-  });
-
-
-  // send title, name and doodle when submit button clicked.
-  $('#submit').on('click', function(e) {
-    // create blob
-    var data = window.atob(canvas.toDataURL().split(',')[1]);
-    var buf = new ArrayBuffer(data.length);
-    var arr = new Uint8Array(buf);
-
-    for(var i = 0; i < data.length; i++) {
-      arr[i] = data.charCodeAt(i);
-    }
-    var blob = new Blob([arr], {type: 'image/png'});
-
-    // send data
-    var formData = new FormData($('form')[0]);
-    formData.append('doodle', blob);
-    // console.log(btoa(formData.get('doodle')));
-    $.ajax({
-      type: 'POST',
-      data: formData,
-      processData: false,
-      contentType: false,
-      url: 'draw', 
-      success: function(data, textStatus, jqXHR) {
-        window.location.href = './';
-      },
-      error: function(req, status, err) {
-        console.log('error');
-        alert('Sorry, error occured...');
-        // document.body.innerHTML = req.responseText;
-      }
-    });
   });
 });
+
+
+function pen_down(x, y)
+{
+  draw(x, y);
+  oldPos.x = x;
+  oldPos.y = y;
+}
+
+function draw(x, y)
+{
+  if (oldPos.x == null && oldPos.y == null) {
+    // beginning of drawing
+    canvas.drawRect({
+      x: x,
+      y: y,
+      width: option.width,
+      height: option.height,
+      fillStyle: option.color
+    });
+  } else {
+    canvas.drawLine({
+      x1: oldPos.x,
+      y1: oldPos.y,
+      x2: x,
+      y2: y,
+      strokeWidth: option.width,
+      strokeStyle: option.color
+    });
+  }
+
+  oldPos.x = x;
+  oldPos.y = y;
+}
+
+function pen_up()
+{
+  oldPos.x = oldPos.y = null;
+}
+
+function changeColor()
+{
+  option.color = $(this).attr('class');
+}
+
+function changeBg()
+{
+  canvas.css('background', 'url("img/' + $(this).val() + '.png")');
+}
+
+function canvas2blob(canvas)
+{
+  var data = window.atob(canvas.toDataURL().split(',')[1]);
+  var arr  = new Uint8Array(new ArrayBuffer(data.length));
+
+  for (var i = 0; i < data.length; i++) {
+    arr[i] = data.charCodeAt(i);
+  }
+
+  return new Blob([ arr ], { type: 'image/png' });
+}
+
+/************************************************************/
+/*                                                          */
+/*  Copyright (C) 2018 Soichiro Kato All Rights Reserved.  */
+/*                                                          */
+/************************************************************/
